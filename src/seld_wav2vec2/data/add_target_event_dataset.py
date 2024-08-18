0710604660063afce565356b9de222af586d2450
orlem <orllem3456@gmail.com>
2024-08-17 23:05:45 -0300

    speed-up target-seld-dataset

--- src/seld_wav2vec2/data/add_target_event_dataset.py
+++ src/seld_wav2vec2/data/add_target_event_dataset.py
@@ -1,5 +1,3 @@
-
-
 import numpy as np
 import torch
 import torch.nn.functional as F
@@ -9,13 +7,17 @@ from seld_wav2vec2.data.transforms import RandomSwapChannel, select_shift_tf
 
 
 def check_r_unit(x, y, z):
-
     r = torch.sqrt(x**2 + y**2 + z**2)
 
     r_sel = r[r != 0]
 
-    assert np.allclose(
-        r_sel, [1.0]*len(r_sel), atol=1e-05), r[0]
+    assert np.allclose(r_sel, [1.0] * len(r_sel), atol=1e-05), r[0]
+
+
+def dict_item_to_tensor(item_dict):
+    for key in item_dict:
+        item_dict[key] = torch.tensor(item_dict[key])
+    return item_dict
 
 
 class AddTargetEventDataset(BaseWrapperDataset):
@@ -25,12 +27,10 @@ class AddTargetEventDataset(BaseWrapperDataset):
         labels,
     ):
         super().__init__(dataset)
-        self.labels = labels
+        self.labels = [dict_item_to_tensor(labels[i]) for i in range(len(labels))]
 
     def get_label(self, index):
-        return (
-            self.labels[index]
-        )
+        return self.labels[index]
 
     def __getitem__(self, index):
         item = self.dataset[index]
@@ -74,20 +74,21 @@ class AddTargetSeldSeqClassDataset(AddTargetEventDataset):
         item["event"] = item_dict["event"]
 
         item_x = torch.zeros(self.nb_classes)
-        item_x[item_dict["labels"]] = torch.tensor(item_dict["x"])
+        item_x[item_dict["labels"]] = item_dict["x"]
         item["x"] = item_x
 
         if "y" in item_dict:
             item_y = torch.zeros(self.nb_classes)
-            item_y[item_dict["labels"]] = torch.tensor(item_dict["y"])
+            item_y[item_dict["labels"]] = item_dict["y"]
             item["y"] = item_y
 
         item_z = torch.zeros(self.nb_classes)
-        item_z[item_dict["labels"]] = torch.tensor(item_dict["z"])
+        item_z[item_dict["labels"]] = item_dict["z"]
         item["z"] = item_z
 
-        labels = F.one_hot(torch.LongTensor(item_dict["labels"]),
-                           num_classes=self.nb_classes)
+        labels = F.one_hot(
+            torch.LongTensor(item_dict["labels"]), num_classes=self.nb_classes
+        )
         labels = labels.sum(dim=0)
         item["labels"] = labels
         return item
@@ -114,10 +115,10 @@ class AddTargetSeldSeqClassDataset(AddTargetEventDataset):
 
         if doa_y:
             collated["doa_labels"] = torch.cat(
-                [torch.stack(x), torch.stack(y), torch.stack(z)], dim=-1)
+                [torch.stack(x), torch.stack(y), torch.stack(z)], dim=-1
+            )
         else:
-            collated["doa_labels"] = torch.cat(
-                [torch.stack(x), torch.stack(z)], dim=-1)
+            collated["doa_labels"] = torch.cat([torch.stack(x), torch.stack(z)], dim=-1)
 
         return collated
 
@@ -133,9 +134,8 @@ class AddTargetSeldAudioFrameClassDataset(AddTargetEventDataset):
         shift_prob=0.5,
         shift_rollover=False,
         n_classes=11,
-        in_channels=4,
+        spectrogram_input=False,
         spectrogram_1d=False,
-
     ):
         super().__init__(dataset, labels)
 
@@ -146,8 +146,10 @@ class AddTargetSeldAudioFrameClassDataset(AddTargetEventDataset):
                 p=doa_swap_prob, n_classes=n_classes
             )
             self.shift_transform = select_shift_tf(
-                p=shift_prob, rollover=shift_rollover,
-                in_channels=in_channels)
+                p=shift_prob,
+                rollover=shift_rollover,
+                spectrogram_input=spectrogram_input,
+            )
         elif doa_swap_augment and shift_augment is False:
             self.swap_transform = RandomSwapChannel(
                 p=doa_swap_prob, n_classes=n_classes
@@ -156,8 +158,10 @@ class AddTargetSeldAudioFrameClassDataset(AddTargetEventDataset):
         elif shift_augment and doa_swap_augment is False:
             self.swap_transform = None
             self.shift_transform = select_shift_tf(
-                p=shift_prob, rollover=shift_rollover,
-                in_channels=in_channels)
+                p=shift_prob,
+                rollover=shift_rollover,
+                spectrogram_input=spectrogram_input,
+            )
         else:
             self.swap_transform = None
             self.shift_transform = None
@@ -166,12 +170,8 @@ class AddTargetSeldAudioFrameClassDataset(AddTargetEventDataset):
         item = self.dataset[index]
 
         item_dict = self.get_label(index)
-
-        sed_labels = torch.tensor(item_dict["sed_labels"])  # (T, N)
-        doa_labels = torch.tensor(item_dict["doa_labels"])  # (T, 3*N)
-
-        item["sed_labels"] = sed_labels
-        item["doa_labels"] = doa_labels
+        item["sed_labels"] = item_dict["sed_labels"]  # (T, N)
+        item["doa_labels"] = item_dict["doa_labels"]  # (T, 3*N)
         return item
 
     def collater(self, samples):
@@ -182,58 +182,63 @@ class AddTargetSeldAudioFrameClassDataset(AddTargetEventDataset):
         sed_labels = [s["sed_labels"] for s in samples if s["id"] in indices]
         doa_labels = [s["doa_labels"] for s in samples if s["id"] in indices]
 
-        sizes_sed = [len(t) for t in sed_labels]
-        target_size_sed = max(sizes_sed)
+        sizes = [len(t) for t in sed_labels]
+        target_size = max(sizes)
 
-        collated_sed_labels = sed_labels[0].new_zeros(len(sed_labels),
-                                                      target_size_sed,
-                                                      sed_labels[0].shape[1])
-
-        for i, (label, size) in enumerate(zip(sed_labels, sizes_sed)):
-            diff = size - target_size_sed
-            if diff == 0:
-                collated_sed_labels[i] = label
-            else:
-                collated_sed_labels[i] = torch.cat(
-                    [label, label.new_full(
-                        (-diff, sed_labels[0].shape[1]), -100)],
-                    dim=0
-                )
-
-        sizes_doa = [len(doa_i) for doa_i in doa_labels]
-        target_size_doa = max(sizes_doa)
+        # (B, T, N)
+        collated_sed_labels = sed_labels[0].new_zeros(
+            len(sed_labels), target_size, sed_labels[0].shape[1]
+        )
 
         # (B, T, 3N)
-        collated_doa_labels = doa_labels[0].new_zeros(len(doa_labels),
-                                                      target_size_doa,
-                                                      doa_labels[0].shape[1])
+        collated_doa_labels = doa_labels[0].new_zeros(
+            len(doa_labels), target_size, doa_labels[0].shape[1]
+        )
 
-        for i, (label, size) in enumerate(zip(doa_labels, sizes_doa)):
-            diff = size - target_size_doa
+        for i, (sed_label, doa_label, size) in enumerate(
+            zip(sed_labels, doa_labels, sizes)
+        ):
+            diff = size - target_size
             if diff == 0:
-                collated_doa_labels[i] = label
+                collated_sed_labels[i] = sed_label
+                collated_doa_labels[i] = doa_label
             else:
+                collated_sed_labels[i] = torch.cat(
+                    [
+                        sed_label,
+                        sed_label.new_full((-diff, sed_labels[0].shape[1]), -100),
+                    ],
+                    dim=0,
+                )
                 collated_doa_labels[i] = torch.cat(
-                    [label, label.new_full(
-                        (-diff, doa_labels[0].shape[1]), 0.0)],
-                    dim=0
+                    [
+                        doa_label,
+                        doa_label.new_full((-diff, doa_labels[0].shape[1]), 0.0),
+                    ],
+                    dim=0,
                 )
 
         if self.shift_transform is not None or self.swap_transform is not None:
             collated_source = collated["net_input"]["source"]
 
             if self.shift_transform is not None:
-                collated_source, collated_sed_labels, collated_doa_labels = self.shift_transform(
-                    collated_source, collated_sed_labels, collated_doa_labels)
+                collated_source, collated_sed_labels, collated_doa_labels = (
+                    self.shift_transform(
+                        collated_source, collated_sed_labels, collated_doa_labels
+                    )
+                )
 
             if self.swap_transform is None:
                 collated["net_input"]["source"] = collated_source
             else:
-                collated_source, collated_sed_labels, collated_doa_labels = self.swap_transform(
-                    collated_source, collated_sed_labels, collated_doa_labels)
+                collated_source, collated_sed_labels, collated_doa_labels = (
+                    self.swap_transform(
+                        collated_source, collated_sed_labels, collated_doa_labels
+                    )
+                )
                 collated["net_input"]["source"] = collated_source
 
-        collated["labels_lengths"] = torch.LongTensor(sizes_sed)
+        collated["labels_lengths"] = torch.LongTensor(sizes)
         collated["sed_labels"] = collated_sed_labels  # (B, T, N)
         collated["ntokens"] = collated["labels_lengths"].sum().item()
         collated["doa_labels"] = collated_doa_labels  # (B, T, 3N)
