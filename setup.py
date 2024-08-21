1374420175d7bb7c4f43d75251103474aa8d14ce
orlem <orllem3456@gmail.com>
2024-08-20 22:30:56 -0300

    update requirements torch

--- setup.py
+++ setup.py
@@ -1,65 +1,18 @@
-import os
-from distutils.core import setup
-
 import numpy as np
 from Cython.Build import cythonize
-from setuptools import find_packages
+from setuptools import find_packages, setup
 
-pkg_dir = os.path.dirname(__name__)
-
-
-requirements = [
-    "autopep8",
-    "pandas==1.5.3",
-    "soundfile==0.12.1",
-    "librosa==0.9.2",
-    "scikit-learn==0.24.2",
-    "matplotlib==3.5.3",
-    "pillow==10.0.1",
-    "spafe==0.3.2",
-    "pyarrow==13.0.0",
-    "torch==1.12.1",
-    "torchvision==0.13.1",
-    "torchaudio==0.12.1",
-    "mpire==2.9.0"
-]
-
-
-dvc_requirements = [
-    "dvclive==2.7.0",
-    "dvc-gdrive==2.19.2"
-]
-
-dev_requirements = [
-    "filelock==3.12.0",
-    "wandb==0.15.3",
-    "importlib-resources==5.12.0",
-    "tensorboardX==2.6.0",
-    "torch_audiomentations==0.11.0",
-    "fairseq @ git+https://github.com/pytorch/fairseq.git@"
-    "4fe8583396191c22011350248119db98ec1b5cb8",
+extensions = [
+    "src/cseld_ambisonics.pyx",
+    "src/seld_wav2vec2/criterions/ccls_feature_class.pyx",
+    "src/seld_wav2vec2/criterions/cSELD_evaluation_metrics.pyx",
 ]
 
 setup(
     name="seld_wav2vec2",
-    version="1.0.0",
     author="Orlem",
-    long_description=open("README.md").read(),
-    packages=find_packages(
-        "src",
-        exclude=[
-            "data*",
-            "models*",
-            "notebooks*",
-            "scripts*",
-        ],
-    ),
+    packages=find_packages("src"),
     package_dir={"": "src"},
-    install_requires=requirements,
-    extras_require={
-        "dvc": dvc_requirements,
-        "dev": dev_requirements,
-    },
-    ext_modules=cythonize("src/cseld_ambisonics.pyx"),
+    ext_modules=cythonize(extensions),
     include_dirs=[np.get_include()],
 )
